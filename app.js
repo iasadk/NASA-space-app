067fa7e27120d6c31fc1940c29b879e97b074192
Anand Yadav <anand55yadav@gmail.com>
2022-10-02 10:36:33 +0530

    globe structure fix

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// const addToCompare = document.getElementById("compare");
-// const globe2 = document.getElementById("globe2")
-// const globeViz = document.getElementById("globeViz");
-// addToCompare.addEventListener("click",()=>{
-//     globe2.classList.toggle("show")
-//     globeViz.classList.toggle("reduce")
 
-// })
